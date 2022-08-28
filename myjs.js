691ddfc5a6a577ce1ed999d804006c775d971b5e
Pavan Reddy <103094199+PAVAN-REDDYY@users.noreply.github.com>
2022-08-28 12:20:47 +0530

    Delete myjs.js

--- myjs.js
+++
@@ -1,8 +0,0 @@
-function leapyear(year){
-   if (year%4==0 && year%100!=0) {
-      console.log(year+" is a leap year");
-   } else {
-      console.log(year+ " is not a leap year");
-   }
-}
-leapyear(2024);
